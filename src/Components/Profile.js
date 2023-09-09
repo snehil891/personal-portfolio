9573df58baf2496f7fc9477fcc220e348ae32d10
snehil891 <snehilsah400@gmail.com>
2023-09-09 12:47:00 +0530

    span color change

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -6,27 +6,29 @@ import Resume from "./Resume.pdf";
 
 export default function Profile() {
   return (
-    <div className="home" id="home">
-      <div className="img">
-        <img src={myImage} about="image" alt="image"></img>
-      </div>
-      <div className="home-content">
-        <h1>
-          Hi I'm <span>Snehil Sah</span>
-        </h1>
-        <div className="text">
-          <h3>Web Developer</h3>
+    <div className="animated-background">
+      <div className="home" id="home">
+        <div className="img">
+          <img src={myImage} about="image" alt="image"></img>
         </div>
-        <p>
-          Aiming to obtain a successful career in the IT Industry and to fully
-          utilize my skills for making a significant contribution to the success
-          of the organization that I will work in.
-        </p>
+        <div className="home-content">
+          <h1>
+            Hi I'm <span>Snehil Sah</span>
+          </h1>
+          <div className="text">
+            <h3>Web Developer</h3>
+          </div>
+          <p>
+            Aiming to obtain a successful career in the IT Industry and to fully
+            utilize my skills for making a significant contribution to the
+            success of the organization that I will work in.
+          </p>
 
-        <div className="btn-box">
-          <a href={Resume} download={"Resume.pdf"} className="btn">
-            Download Resume
-          </a>
+          <div className="btn-box">
+            <a href={Resume} download={"Resume.pdf"} className="btn">
+              Download Resume
+            </a>
+          </div>
         </div>
       </div>
     </div>
